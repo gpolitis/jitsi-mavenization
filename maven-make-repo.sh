e74c12f2ce608bed11d11f1d0878530ac8952ea4
George Politis <gp@jitsi.org>
2014-12-11 19:13:56 +0100

    Up maven-make-repo.sh

--- maven-make-repo.sh
+++ maven-make-repo.sh
@@ -1,138 +1,78 @@
 #!/bin/sh -e
 
-REPO=$HOME/.m2/repository
+MAVEN_REPO="$(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '[INFO]')"
+GROUP_ID=org.jitsi
 
-JITSI_HOME=$HOME/bluejimp/src
-
-JITSI_BUNDLES=$JITSI_HOME/jitsi/sc-bundles
-JITSI_LIBS_INSTALLER_EXCLUDE=$JITSI_HOME/jitsi/lib/installer-exclude
-JITSI_LIBS_MAC=$JITSI_HOME/jitsi/lib/os-specific/mac
-LIBJITSI_LIBS=$JITSI_HOME/libjitsi/lib
-JVB_LIBS=$JITSI_HOME/jitsi-videobridge/lib
-
-FORCE_LIB_UPDATE=0
-FORCE_BUNDLE_UPDATE=1
-
-build_jitsi() {
-	local curpath="$(pwd)"
-	cd "$JITSI_HOME/jitsi"
-	ant rebuild
-	cd "$curpath"
-}
+# Adjust the following 3 variables to suit your environment.
+JITSI_HOME="$HOME/BlueJimp/src/org.jitsi.sipcomm"
+LIBJITSI_HOME="$HOME/BlueJimp/src/org.jitsi.libjitsi"
+JVB_HOME="$HOME/BlueJimp/src/org.jitsi.videobridge"
 
 install_lib() {
-	local file=$1
-	local artifactId=$2
-	local version=$3
+	local FILE="$1"
+	local ARTIFACT_ID="$2"
+	local VERSION="$3"
 
-	# TODO check that the file exists.
-
-	if [ -z "$version" ] ; then
-		version='1.0-SNAPSHOT'
+	if [ -z "$VERSION" ] ; then
+		VERSION='1.0-SNAPSHOT'
 	fi
-
-	if [ -f $REPO/local/jitsi/libs/$artifactId/$version/$artifactId-$version.jar -a "$FORCE_LIB_UPDATE" != "1" ]
-	then
-		return
+	local INSTALLED_FILE="$MAVEN_REPO/${GROUP_ID/\.//}/$ARTIFACT_ID/$VERSION/$ARTIFACT_ID-$VERSION.jar"
+	if ! cmp "$FILE" "$INSTALLED_FILE" >/dev/null 2>&1; then
+		mvn install::install-file \
+			-DcreateChecksum=true \
+			-Dpackaging=jar \
+			-Dfile="$FILE" \
+			-DgroupId="$GROUP_ID" \
+			-DartifactId="$ARTIFACT_ID" \
+			-Dversion="$VERSION" >/dev/null
+		
+		echo "Installed $INSTALLED_FILE"
 	fi
-
-	mvn install::install-file \
-		-DcreateChecksum=true \
-		-Dpackaging=jar \
-		-Dfile="$file" \
-		-DgroupId=local.jitsi.libs \
-		-DartifactId="$artifactId" \
-		-Dversion="$version"
 }
 
-install_bundle() {
-	local file=$JITSI_BUNDLES/$1.jar
-	local artifactId=$1
-	local version=$3
+# install jitsi libs
+install_lib $JITSI_HOME/lib/installer-exclude/ymsg_network_v0_67.jar ymsg-network 0.6.7
+install_lib $JITSI_HOME/lib/installer-exclude/profiler4j-1.0-beta3-SC.jar profiler4j 1.0-beta3
+install_lib $JITSI_HOME/lib/installer-exclude/joscar-client.jar joscar-client 
+install_lib $JITSI_HOME/lib/installer-exclude/joscar-common.jar joscar-common
+install_lib $JITSI_HOME/lib/installer-exclude/joscar-protocol.jar joscar-protocol
+install_lib $JITSI_HOME/lib/installer-exclude/jmork-1.0.5-SNAPSHOT.jar jmork 1.0.5-SNAPSHOT
+install_lib $JITSI_HOME/lib/installer-exclude/smack.jar smack
+install_lib $JITSI_HOME/lib/installer-exclude/smackx.jar smackx
+install_lib $JITSI_HOME/lib/installer-exclude/smackx-debug.jar smackx-debug
+install_lib $JITSI_HOME/lib/installer-exclude/laf-widget.jar laf-widget 4.0
+install_lib $JITSI_HOME/lib/installer-exclude/jnsapi.jar jnsapi 
+install_lib $JITSI_HOME/lib/installer-exclude/jmyspell-core.jar jmyspell-core 1.0.0-beta-2
+install_lib $JITSI_HOME/lib/installer-exclude/jain-sip-ri.jar jain-sip-ri 1.2.159
+install_lib $JITSI_HOME/lib/installer-exclude/jain-sdp.jar jain-sdp 1.0.159
+install_lib $JITSI_HOME/lib/installer-exclude/jain-sip-api.jar jain-sip-api 1.2
+install_lib $JITSI_HOME/lib/installer-exclude/dict4j.jar dict4j 
+install_lib $JITSI_HOME/lib/installer-exclude/dhcp4java-1.00.jar dhcp4java 1.00
+install_lib $JITSI_HOME/lib/installer-exclude/dnsjava.jar dnsjava 2.1.6
+install_lib $JITSI_HOME/lib/installer-exclude/swing-worker-1.2.jar swing-worker 1.2
+install_lib $JITSI_HOME/lib/installer-exclude/jmdns.jar jmdns
+install_lib $JITSI_HOME/lib/installer-exclude/gdata-core-1.0.jar gdata-core 1.0
+install_lib $JITSI_HOME/lib/installer-exclude/gdata-client-1.0.jar gdata-client 1.0
+install_lib $JITSI_HOME/lib/installer-exclude/gdata-client-meta-1.0.jar gdata-client-meta 1.0
+install_lib $JITSI_HOME/lib/installer-exclude/gdata-contacts-3.0.jar gdata-contacts 3.0
+install_lib $JITSI_HOME/lib/installer-exclude/gdata-contacts-meta-3.0.jar gdata-contacts-meta 3.0
+install_lib $JITSI_HOME/lib/installer-exclude/libdbus-java-2.7.jar libdbus-java 2.7
+install_lib $JITSI_HOME/lib/installer-exclude/jdic_misc.jar jdic-misc
+install_lib $JITSI_HOME/lib/installer-exclude/jna.jar jna 3.2.7
+install_lib $JITSI_HOME/lib/installer-exclude/jna-platform.jar jna-platform 3.2.7
 
-	# TODO check that the file exists.
+# install mac specific jitsi libs
+install_lib $JITSI_HOME/lib/installer-exclude/os-specific/mac/growl4j.jar growl4j 
+install_lib $JITSI_HOME/lib/installer-exclude/os-specific/mac/OrangeExtensions.jar AppleJavaExtensions
 
-	if [ -z "$version" ] ; then
-		version='1.0-SNAPSHOT'
-	fi
+# install libjitsi libs
+install_lib $LIBJITSI_HOME/lib/jspeex.jar jspeex 0.9.7
+install_lib $LIBJITSI_HOME/lib/zrtp4j-light.jar zrtp4j-light 
+install_lib $LIBJITSI_HOME/lib/bccontrib-1.0-SNAPSHOT.jar bccontrib 
 
-	if [ -f $REPO/local/jitsi/bundles/$artifactId/$version/$artifactId-$version.jar -a "$FORCE_BUNDLE_UPDATE" != "1" ]
-	then
-		return
-	fi
-
-	mvn install::install-file \
-		-DcreateChecksum=true \
-		-Dpackaging=jar \
-		-Dfile="$file" \
-		-DgroupId=local.jitsi.bundles \
-		-DartifactId="$artifactId" \
-		-Dversion="$version"
-}
-
-
-# jitsi libs
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/ymsg_network_v0_67.jar ymsg-network 0.6.7
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/profiler4j-1.0-beta3-SC.jar profiler4j 1.0-beta3
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/joscar-client.jar joscar-client 
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/joscar-common.jar joscar-common
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/joscar-protocol.jar joscar-protocol
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jmork-1.0.5-SNAPSHOT.jar jmork 1.0.5-SNAPSHOT
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/smack.jar smack
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/smackx.jar smackx
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/smackx-debug.jar smackx-debug
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/laf-widget.jar laf-widget 4.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jnsapi.jar jnsapi 
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jmyspell-core.jar jmyspell-core 1.0.0-beta-2
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jain-sip-ri.jar jain-sip-ri 1.2.159
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jain-sdp.jar jain-sdp 1.0.159
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jain-sip-api.jar jain-sip-api 1.2
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/dict4j.jar dict4j 
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/dhcp4java-1.00.jar dhcp4java 1.00
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/dnsjava.jar dnsjava 2.1.6
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/swing-worker-1.2.jar swing-worker 1.2
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jmdns.jar jmdns
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/gdata-core-1.0.jar gdata-core 1.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/gdata-client-1.0.jar gdata-client 1.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/gdata-client-meta-1.0.jar gdata-client-meta 1.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/gdata-contacts-3.0.jar gdata-contacts 3.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/gdata-contacts-meta-3.0.jar gdata-contacts-meta 3.0
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/libdbus-java-2.7.jar libdbus-java 2.7
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jdic_misc.jar jdic-misc
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jna.jar jna 3.2.7
-install_lib $JITSI_LIBS_INSTALLER_EXCLUDE/jna-platform.jar jna-platform 3.2.7
-
-# libjitsi
-install_lib $LIBJITSI_LIBS/jspeex.jar jspeex 0.9.7
-install_lib $LIBJITSI_LIBS/zrtp4j-light.jar zrtp4j-light 
-install_lib $LIBJITSI_LIBS/bccontrib-1.0-SNAPSHOT.jar bccontrib 
-
-# jitsi mac libs
-install_lib $JITSI_LIBS_MAC/growl4j.jar growl4j 
-install_lib $JITSI_LIBS_MAC/OrangeExtensions.jar AppleJavaExtensions
-
-# jitsi videobridge libs
-install_lib $JVB_LIBS/whack.jar whack
-install_lib $JVB_LIBS/xpp3.jar xpp3
-install_lib $JVB_LIBS/jitsi-android-osgi.jar osgi-android
-install_lib $JVB_LIBS/sigar.jar sigar 1.6.4
-
-# jitsi bundles
-build_jitsi
-install_bundle util
-install_bundle configuration
-install_bundle sysactivitynotifications
-install_bundle dnsservice
-install_bundle neomedia
-install_bundle credentialsstorage
-install_bundle addrbook
-install_bundle certificate
-install_bundle smacklib
-install_bundle customavatar-service
-install_bundle ui-service
-install_bundle argdelegation-service
-install_bundle googlecontacts-service
-install_bundle globaldisplaydetails
-install_bundle hid-service
-install_bundle resourcemanager
+# install jitsi videobridge libs
+install_lib $JVB_HOME/lib/whack.jar whack
+install_lib $JVB_HOME/lib/xpp3.jar xpp3
+install_lib $JVB_HOME/lib/jitsi-android-osgi.jar osgi-android
+install_lib $JVB_HOME/lib/sigar.jar sigar 1.6.4
 
